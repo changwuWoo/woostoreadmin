344d3063fa87de3960de8055631bc18f53f73a59
valiantwu <changwuWoo@outlook.com>
2018-04-25 12:09:57 +0800

    Update tablespace.sql

--- doc/tablespace.sql
+++ doc/tablespace.sql
@@ -1,4 +1,7 @@
 create tablespace woostoreadmin_tablespace
+datafile 'E:\app\oracle\oradata\dev\woostoreadmin_tablespace.dbf'
+size 200M autoextend on  next 2M  maxsize 800M;
+create tablespace woostoreadmin_tablespace
 datafile 'D:\oracle\tablespace\woostoreadmin_tablespace.dbf'
 size 1M autoextend on  next 2M  maxsize 50M;
 create user woostoreadmin identified by woostoreadmin
